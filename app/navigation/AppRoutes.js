da12b8dceeb9f7843703d874f8856665357092b9
alexandre <alexandre_pereira33@hotmail.com>
2021-10-04 23:18:22 -0300

    Ajustando arquivos com tela de plataformas

--- app/navigation/AppRoutes.js
+++ app/navigation/AppRoutes.js
@@ -1,4 +1,5 @@
 export const ROUTE_BRAZIL_NEWS = "brazil-news"
+export const ROUTE_PLATFORMS = "platforms"
 
 export const ROUTE_HOME = "home"
 export const ROUTE_NEW_DETAILS = "new-details"
--- app/navigation/StackNavigation.js
+++ app/navigation/StackNavigation.js
@@ -3,7 +3,8 @@ import { createStackNavigator } from '@react-navigation/stack'
 import { StatusBar, useTheme } from 'native-base'
 import React from 'react'
 import BrazilNews from '../screens/brazilnews/BrazilNews'
-import { ROUTE_HOME, ROUTE_BRAZIL_NEWS } from './AppRoutes'
+import Platforms from '../screens/platforms/Platforms'
+import { ROUTE_HOME, ROUTE_BRAZIL_NEWS, ROUTE_PLATFORMS } from './AppRoutes'
 import TabNavigation from './TabNavigation'
 
 const Stack = createStackNavigator()
@@ -19,6 +20,8 @@ const StackNavigation = () => {
                     options={{ headerShown: false }} />
                 <Stack.Screen name={ROUTE_BRAZIL_NEWS} component={BrazilNews} 
                     options={{ title: 'Brazil News'}} />
+                 <Stack.Screen name={ROUTE_PLATFORMS} component={Platforms} 
+                    options={{ title: 'Platforms'}} />
             </Stack.Navigator>
         </NavigationContainer>
     )
